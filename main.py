694e0cf2fee46f22cd07ebfabd172a047ac3ceec
Kiolesis <78695820+kiolesis@users.noreply.github.com>
2021-08-25 22:07:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # Like and subscribe!
 # https://www.youtube.com/watch?v=-lefbawmaFk
-import os
-import hashlib
- 
-print("Kiolesis Studio Code | Developer Tools")
-print("")
-print("Hashowanie wartości w SHA256")
-zmiennahash = input(":")
- 
-print(hashlib.sha256(zmiennahash.encode('utf-8')).hexdigest())
-os.system("pause>>nul")
-os.system("exit")
+import os # Import OS
+import hashlib # Import HASHLIB
+
+print("Narzędzie Secure Hash Algorithm 256") # Nazwij swój program dowolną nazwą, niech to nie będzie podobne do czegoś powszechnego
+print("") # Wolna linijka tekstu
+print("Hashowanie wartości w SHA256") # Informacja dla użytkownika o funkcji programu
+zmiennahash = input(":") # Przyjmowanie danych od użytkownika
+
+print(hashlib.sha256(zmiennahash.encode('utf-8')).hexdigest()) # Wyprintuj zmiennahash, ale użyj hashlib w celu zahashowania sha256.
+os.system("pause>>nul") # Opcjonalny kod tak na prawdę, jeśli jednak korzystasz z programu z poziomu linii poleceń, to zostaw ten kod.
+os.system("exit") # Opcjonalnykod tak na prawdę tjw.
